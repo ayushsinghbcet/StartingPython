c09befddf93a13c42c3ba6d0b2b2a50e40f4c838
AYUSH <35888683+ayushsinghbcet@users.noreply.github.com>
2024-05-29 22:30:36 +0530

    Create 1. Hello World.py

---
+++ 1. Hello World.py
@@ -0,0 +1,15 @@
+//Guess the output of the below code
+// With single quotes
+print('hello, world')
+
+// With double quotes
+print("hello, world")
+
+// Without paranthesis
+print 'hello, world'
+
+//Without Quotes
+//print(hello, world)
+
+//Without Quotes & comma
+//print(hello world)
